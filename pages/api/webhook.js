a7ee51d563749e64caea1a26700aadbf3e415ab1
Fawwaz <fawwaz.chowdhury@googlemail.com>
2024-12-01 22:03:47 +0000

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,7 +2,17 @@ import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
 // https://console.firebase.google.com/u/1/project/amzn-2/settings/serviceaccounts/adminsdk
-var serviceAccount = require("../../../permissions.json");
+var serviceAccount = require(
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,);
 
 const app = !admin.apps.length
   ? admin.initializeApp({
